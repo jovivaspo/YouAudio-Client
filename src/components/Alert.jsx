f3a4f2de5097001cd3dc347d21cd403c7e5fdea0
jovivaspo <jovivaspo@gmail.com>
2023-04-08 08:43:02 +0200

    Fixing alert effect

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -3,30 +3,40 @@ import { Globalcontext } from "../contexts/GlobalContext";
 
 const Alert = () => {
   const { alert, setAlert } = useContext(Globalcontext);
-  const ref = useRef(null);
+  const refContainer = useRef(null);
 
   useEffect(() => {
-    if (ref.current) {
-      if (alert) {
-        ref.current.style.transform = "translateX(0)";
-        const timeAlert = setTimeout(() => {
-          ref.current.style.transform = "translateX(100%)";
-          setAlert("");
-        }, 2500);
-        return () => clearTimeout(timeAlert);
-      }
+    if (refContainer.current && alert) {
+      refContainer.current.style.transform = "translateX(0)";
+      const timeAlert = setTimeout(() => {
+        refContainer.current.style.transform = "translateX(100%)";
+      }, 2500);
+      return () => clearTimeout(timeAlert);
     }
-  }, [alert, ref]);
+  }, [alert, refContainer.current]);
+
+  useEffect(() => {
+    if (alert) {
+      const deleteAlert = setTimeout(() => {
+        setAlert("");
+      }, 3200);
+      return () => clearTimeout(deleteAlert);
+    }
+  }, [alert]);
 
   return (
-    <div
-      ref={ref}
-      className="absolute top-20 right-0 bg-blue-600  p-4 rounded-l-xl translate-x-full ease-in duration-100 pr-6"
-    >
-      <p className="text-white">
-        <strong>{alert}</strong>
-      </p>
-    </div>
+    <>
+      {alert && (
+        <div
+          ref={refContainer}
+          className="absolute top-20 right-0 bg-blue-600  p-4 rounded-l-xl translate-x-full ease-in duration-100 pr-6"
+        >
+          <p className="text-alert text-white">
+            <strong>{alert}</strong>
+          </p>
+        </div>
+      )}
+    </>
   );
 };
 
--- src/components/VideoItem.jsx
+++ src/components/VideoItem.jsx
@@ -22,21 +22,20 @@ const VideoItem = ({ video }) => {
 
   return (
     <div className="flex flex-col justify-start items-center w-[340px] h-[280px] sm:w-[320px] sm:h-[270px] lg:w-[280px] lg:h-[260px]">
-      <div className="position relative bg-slate-800 min-h-[160px] w-[320px] sm:w-[300px] lg:w-[280px]  rounded-lg">
-      <img
-        src={
-          video.snippet?.thumbnails.medium.url ||
-          video.thumbnail ||
-          video.thumbnails[1].url ||
-          ""
-        }
-        alt=""
-        className=" position absolute top-0 right-0 rounded-xl cursor-pointer w-full h-full object-cover"
-        loading="lazy"
-        onClick={handlerClick}
-      />
+      <div className="position relative bg-slate-800 min-h-[160px] w-[320px] sm:w-[300px] lg:w-[280px]  rounded-xl">
+        <img
+          src={
+            video.snippet?.thumbnails.medium.url ||
+            video.thumbnail ||
+            video.thumbnails[1].url ||
+            ""
+          }
+          alt=""
+          className=" position absolute top-0 right-0 rounded-xl cursor-pointer w-full h-full object-cover"
+          loading="lazy"
+          onClick={handlerClick}
+        />
       </div>
-    
 
       <div className="flex flex-col justify-start w-full my-2">
         <p className="text-white cursor-pointer text-md" onClick={handlerClick}>
